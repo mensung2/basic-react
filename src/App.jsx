784642df5ee8ddd3f6a4472cd89f5b87011dca1c
minseo <mensung6@gmail.com>
2024-05-27 20:27:28 +0900

    homework submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,43 @@
-function App() {}
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const plusCount = () => {
+    setCount(count + 1);
+  };
+
+  const miunsCount = () => {
+    setCount(count - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  return (
+    <>
+      <div className="mainPost">
+        <div className="countSet">
+          <div className="top">
+            <button onClick={miunsCount} className="miunsCount">
+              -
+            </button>
+            <p>Count: {count}</p>
+            <button onClick={plusCount} className="plusCount">
+              +
+            </button>
+          </div>
+          <div className="bottom">
+            <button onClick={resetCount} className="resetCount">
+              reset
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default App;
